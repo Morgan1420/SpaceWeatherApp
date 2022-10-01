44926d09829d59d9a0e6f13695c7f04761198006
Morgan <jan.moran.r@gmail.com>
2022-10-01 19:54:04 +0200

    Hola

--- app/src/main/java/com/example/spaceweatherapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/spaceweatherapp/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.spaceweatherapp.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,30 +9,28 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.spaceweatherapp.databinding.FragmentHomeBinding
+import android.widget.Button
+import android.widget.ImageView
+import com.example.spaceweatherapp.NavBarActivity
+import com.example.spaceweatherapp.R
 
 class HomeFragment : Fragment() {
 
+    val earthStatsButton = getView()?.findViewById<ImageView>(R.id.earthImage)
     private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View
+    {
+        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
         return root
     }
 
